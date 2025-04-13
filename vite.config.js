ce2e833239474e5cafc1194642c4c179abe75f13
sebfried <dev@sebastianfriedrich.at>
2025-04-13 11:15:40 +0200

    refactor(vite): Remove host option from server configuration

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
-    host: true, // Allow exposing via network addresses
     allowedHosts: [
       'm4.local', // Allow specific hostname
       // Add other hostnames if needed
