df3c0ae880739b9d5b0fa2ff1a85bbd6b9a973cd
sebfried <dev@sebastianfriedrich.at>
2025-04-13 19:55:11 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Base directory for GitHub Pages deployment (repository name)
-  base: '/drive/',
-  build: {
-    // Output directory for the build
-    outDir: 'docs',
-  },
   server: {
     allowedHosts: [
       'm4.local', // Allow specific hostname
